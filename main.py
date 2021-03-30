4f60188836f96f3763508cd8b9d774fdaad780d1
Adrian Millan <adrian.millan@voseda.com>
2021-03-30 12:17:53 -0600

    Consumimos la API para obtener los dispositivos disponibles, solicitamos el dispositivo a consultar y obtenemos la información de la política de acceso del dispositivo seleccionado

--- main.py
+++ main.py
@@ -14,6 +14,68 @@ def getToken(account, password):
     return accessToken
 
 
+def getDevices(token):
+    URL = 'https://10.10.10.54/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devices/devicerecords'
+    HEADERS = {'Content-Type': 'application/json'}
+
+    HEADERS['X-auth-access-token'] = token
+
+    response = requests.get(URL, headers=HEADERS, verify=False)
+
+    response= response.json()
+    items= response["items"]
+    print("List of devices")
+    for item in items:
+        print(item["id"])
+
+    device = input("select a device: ")
+
+    getAccesPolicy(token,device)
+
+def getAccesPolicy(token, idDevice):
+    URL = 'https://10.10.10.54/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/assignment/policyassignments?expanded=true'
+    HEADERS = {'Content-Type': 'application/json'}
+
+    HEADERS['X-auth-access-token'] = token
+
+    response = requests.get(URL, headers=HEADERS, verify=False)
+    response = response.json()
+
+    accessPolicy=0
+    items = response["items"]
+    for item in items:
+        if item["policy"]["type"] == "AccessPolicy":
+            targetDevices = item["targets"]
+            for targetDevice in targetDevices:
+                if targetDevice["id"] == idDevice:
+                    accessPolicy=item["policy"]["id"]
+
+    if accessPolicy == 0 : 
+        print ("there aren't access policies for this device")
+    else:
+        detailAccessPolicy(token, accessPolicy)
+
+def detailAccessPolicy(token, idAccesPolicy):
+
+    URL = f'https://10.10.10.54/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/assignment/policyassignments/{idAccesPolicy}'
+
+    HEADERS = {'Content-Type': 'application/json'}
+
+    HEADERS['X-auth-access-token'] = token
+
+    response = requests.get(URL, headers=HEADERS, verify=False)
+    response = response.json()
+    policy= response["policy"]
+    policyType = policy["type"]
+    #policyDescription = policy["description"]
+    policyName= policy["name"]
+    policyId= policy["id"]
+
+    print(f"Access Policy for this device:\nType: {policyType}\nName: {policyName}\nId: {policyId}")
+
+
+
 account=input("Usuario: ")
 password= input("Password: ")
-print(getToken(account,password))
+getDevices(getToken(account, password))
+
